b0b6bd4f686ba18758ffbc2f9b4111914c2164f3
locoabuelito <45473720+locoabuelito@users.noreply.github.com>
2020-05-18 21:28:07 -0400

    Creación de projecto. Primer commit

---
+++ src/main/java/edu/py/octodb/OctodbApplication.java
@@ -0,0 +1,13 @@
+package edu.py.octodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctodbApplication.class, args);
+	}
+
+}
